0dd14e06e9934c52ce5de383ffd15449abf9ad3c
tawhid12 <tawhid99@gmail.com>
2024-04-16 01:27:44 +0600

    correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -246,6 +246,11 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Login</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('register') }}">Register |
+                                Sign
+                                Up</a>
+                        </li>
                     @endif
                 </ul>
             </div>
