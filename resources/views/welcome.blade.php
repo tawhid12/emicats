09156357fa5f7c47d5d61cf91985abd49bc65a66
tawhid12 <tawhid99@gmail.com>
2024-04-15 17:59:52 +0600

    correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -243,7 +243,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title">Pt</h5>
-                        <h5 class="card-details m-0">$ {{ $setting->pt }}</h5>
+                        <h5 class="card-details m-0">USD $ {{ $setting->pt }}</h5>
                     </div>
                 </div>
             </div>
@@ -251,7 +251,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title">Pd</h5>
-                        <h5 class="card-details m-0">$ {{ $setting->pd }}</h5>
+                        <h5 class="card-details m-0">USD $ {{ $setting->pd }}</h5>
                     </div>
                 </div>
             </div>
@@ -259,7 +259,7 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <h5 class="card-title">Rh</h5>
-                        <h5 class="card-details m-0">$ {{ $setting->rh }}</h5>
+                        <h5 class="card-details m-0">USD $ {{ $setting->rh }}</h5>
                     </div>
                 </div>
             </div>
